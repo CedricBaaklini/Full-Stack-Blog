c1e7d5ca6bc90aa9cb020dee9bbd404f0f06b6aa
Cedric Baaklini <cabaaklini@icloud.com>
2024-02-23 12:51:12 -0500

    Creating frontend

---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="post" v-if="post">
+    <h2> {{post.title}}: {{post.subtitle}} </h2>
+    By <AuthorLink :author="post.author"/>
+    <div>
+      {{displayableDate(post.publishDate)}}
+    </div>
+    <p class="post__description">
+      {{post.metaDescription}}
+    </p>
+    <article>
+      {{post.body}}
+    </article>
+    <ul>
+      <li class="post__tags" v-for="tag in post.tags" :key="tag.name">
+        <router-link :to="`/tag/${tag.name}`">
+          {{tag.name}}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import AuthorLink from "@/components/AuthorLink.vue";
+
+export default {
+  name: 'Post',
+  components: {
+    AuthorLink,
+  },
+  data() {
+    return {
+      post: null,
+    }
+  },
+  methods: {
+    displayableDate(date) {
+      return new Intl.DateTimeFormat(
+          'en-Us',
+          {dateStyle: 'full'},
+      ).format(new Date(date))
+    }
+  },
+}
+</script>
