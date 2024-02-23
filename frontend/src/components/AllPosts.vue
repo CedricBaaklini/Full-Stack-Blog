6fccfacf551553903cfb70a94e7eada4a4c9769b
Cedric Baaklini <cabaaklini@icloud.com>
2024-02-23 01:28:59 -0500

    Creating frontend

---
+++ frontend/src/components/AllPosts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2> Recent Posts </h2>
+    <PostList v-if="allPosts" :posts="allPosts"/>
+  </div>
+</template>
+
+<script setup>
+import PostList from "@/components/PostList.vue";
+
+export default {
+  name: 'AllPosts',
+  components: {
+    PostList,
+  },
+  data() {
+    return {
+      allPosts: null,
+    }
+  },
+}
+</script>
